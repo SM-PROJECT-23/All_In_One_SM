afde4317dbdb40d883458fd5197517f0600fcf5b
Ratrick1ng <a38386@alunos.ipb.pt>
2023-06-17 22:02:43 +0100

    Account and Payment Pages Done

--- app/src/main/java/com/example/all_in_one_sm/SuccessfullPayment.kt
+++ app/src/main/java/com/example/all_in_one_sm/SuccessfullPayment.kt
@@ -1,4 +1,12 @@
 package com.example.all_in_one_sm
 
-class SuccessfullPayment {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SuccessfullPayment: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.successfulpayment)
+    }
 }
