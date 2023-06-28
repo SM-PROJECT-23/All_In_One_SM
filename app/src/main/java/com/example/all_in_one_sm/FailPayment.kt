98e38cf063a61e38b7903c53c8daf4d85abbd4df
Ratrick1ng <a38386@alunos.ipb.pt>
2023-06-28 19:01:10 +0100

    Navigation: Fail Payment -> Shopping Bag

--- app/src/main/java/com/example/all_in_one_sm/FailPayment.kt
+++ app/src/main/java/com/example/all_in_one_sm/FailPayment.kt
@@ -1,4 +1,20 @@
 package com.example.all_in_one_sm
 
-class FailPayment {
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class FailPayment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.failpayment)
+
+        val returnButton: Button = findViewById(R.id.return_button)
+        returnButton.setOnClickListener {
+            // Navigate to the ShoppingBag activity
+            val intent = Intent(this, ShoppingBag::class.java)
+            startActivity(intent)
+        }
+    }
 }
