40241d49f10f04bad04ac10b66b76fe4af2f683e
TedElbow <tedelbow@gmail.com>
2023-06-30 14:48:38 +0100

    Ted

---
+++ app/src/main/java/com/example/all_in_one_sm/ItemUser.kt
@@ -0,0 +1,42 @@
+package com.example.all_in_one_sm
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.all_in_one_sm.ui.theme.All_In_One_SMTheme
+
+data class ItemUserData(
+    val name: String,
+    val username: String,
+    val email: String,
+    val country: String,
+)
+
+class ItemUser : ComponentActivity() {
+    private lateinit var usernameTextView: TextView
+    private lateinit var emailTextView: TextView
+    private lateinit var phoneTextView: TextView
+    private lateinit var countryTextView: TextView
+
+    private fun navigateToItem() {
+        val intent = Intent(this, YourItemPage::class.java)
+        startActivity(intent)
+    }
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.profile)
+    }
+}
